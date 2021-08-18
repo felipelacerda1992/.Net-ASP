bff4b8d36d0f38118d814f48b2ec48337f5878a2
felipelacerda1992 <89145658+felipelacerda1992@users.noreply.github.com>
2021-08-18 12:40:20 -0300

    Add files via upload

---
+++ Financeiro/FinanceiroWeb/PageBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinanceiroWeb
+{
+    public class PageBase : Controller
+    {
+        public int CodigoLogado
+        {
+            get
+            {
+                return Session["cod"] == null ? 0 : Convert.ToInt32(Session["cod"]);
+            }
+            set
+            {
+                Session["cod"] = value;
+            }
+        }
+    }
+}
